094ebb9fcecf29664baea3a21f812b4371c7b925
root <assis.ayoub@gmail.com>
2021-08-15 17:55:26 +0000

    Install necessary deps

---
+++ install_requirements.sh
@@ -0,0 +1,12 @@
+pip install --upgrade --quiet jupyterlab jupyterlab-git
+echo "==> Jupyterlab installed successfully."
+pip install torchvision torchtext torch --upgrade --quiet
+echo "==> Pytorch installed successfully."
+pip install albumentations --upgrade --quiet
+echo "==> Albumentations installed successfully."
+pip install timm --upgrade --quiet
+echo "==> Timm installed successfully."
+pip install pytorch-lightning --upgrade --quiet
+echo "==> Pytorch-Lightning installed successfully."
+pip install torchmetrics --upgrade --quiet
+echo "==> Torchmetrics installed successfully."
