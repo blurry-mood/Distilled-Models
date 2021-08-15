7bb7583cabb268fe6b844d2f0cd6757e0bfd8ab4
root <assis.ayoub@gmail.com>
2021-08-15 18:47:25 +0000

    install tqdm

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,12 +1,25 @@
 pip install --upgrade --quiet jupyterlab jupyterlab-git
 echo "==> Jupyterlab installed successfully."
+
 pip install torchvision torchtext torch --upgrade --quiet
 echo "==> Pytorch installed successfully."
+
+pip install Pillow --upgrade --quiet
+echo "==> PIL installed successfully."
+
 pip install albumentations --upgrade --quiet
 echo "==> Albumentations installed successfully."
+
 pip install timm --upgrade --quiet
 echo "==> Timm installed successfully."
+
 pip install pytorch-lightning --upgrade --quiet
 echo "==> Pytorch-Lightning installed successfully."
+
 pip install torchmetrics --upgrade --quiet
 echo "==> Torchmetrics installed successfully."
+
+pip install tqdm --upgrade --quiet
+conda install -c conda-forge ipywidgets
+jupyter nbextension enable --py widgetsnbextension
+echo "==> Tqdm installed successfully."
