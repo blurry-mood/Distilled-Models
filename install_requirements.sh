f2042f2b047e9bd91b3edcf33e0fe72c181e8a30
root <assis.ayoub@gmail.com>
2021-08-15 22:19:06 +0000

    Fix pytorch-lightning & apex issue

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,25 +1,19 @@
-pip install --upgrade --quiet jupyterlab jupyterlab-git
+pip install --quiet jupyterlab jupyterlab-git
 echo "==> Jupyterlab installed successfully."
 
-pip install torchvision torchtext torch --upgrade --quiet
-echo "==> Pytorch installed successfully."
-
-pip install Pillow --upgrade --quiet
+pip install Pillow --quiet
 echo "==> PIL installed successfully."
 
-pip install albumentations --upgrade --quiet
+pip install albumentations --quiet
 echo "==> Albumentations installed successfully."
 
-pip install timm --upgrade --quiet
+pip install timm --quiet
 echo "==> Timm installed successfully."
 
-pip install pytorch-lightning --upgrade --quiet
+pip install pytorch-lightning --quiet
 echo "==> Pytorch-Lightning installed successfully."
 
-pip install torchmetrics --upgrade --quiet
-echo "==> Torchmetrics installed successfully."
-
-pip install tqdm --upgrade --quiet
+pip install tqdm --quiet
 conda install -c conda-forge ipywidgets
 jupyter nbextension enable --py widgetsnbextension
 echo "==> Tqdm installed successfully."
