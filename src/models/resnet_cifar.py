5e59eb1d3a0a7669cc48951a65195479076cb794
root <assis.ayoub@gmail.com>
2021-09-24 22:18:27 +0000

    include resnet32 impelementation author

--- src/models/resnet_cifar.py
+++ src/models/resnet_cifar.py
@@ -1,3 +1,27 @@
+'''
+Properly implemented ResNet-s for CIFAR10 as described in paper [1].
+The implementation and structure of this file is hugely influenced by [2]
+which is implemented for ImageNet and doesn't have option A for identity.
+Moreover, most of the implementations on the web is copy-paste from
+torchvision's resnet and has wrong number of params.
+Proper ResNet-s for CIFAR10 (for fair comparision and etc.) has following
+number of layers and parameters:
+name      | layers | params
+ResNet20  |    20  | 0.27M
+ResNet32  |    32  | 0.46M
+ResNet44  |    44  | 0.66M
+ResNet56  |    56  | 0.85M
+ResNet110 |   110  |  1.7M
+ResNet1202|  1202  | 19.4m
+which this implementation indeed has.
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+[2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+If you use this implementation in you work, please don't forget to mention the
+author, Yerlan Idelbayev.
+'''
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -88,25 +112,25 @@ class ResNet(nn.Module):
         return out
 
 
-def resnet20():
-    return ResNet(BasicBlock, [3, 3, 3])
+def resnet20(num_classes=100):
+    return ResNet(BasicBlock, [3, 3, 3], num_classes=num_classes)
 
 
-def resnet32():
-    return ResNet(BasicBlock, [5, 5, 5])
+def resnet32(num_classes=100):
+    return ResNet(BasicBlock, [5, 5, 5], num_classes=num_classes)
 
 
-def resnet44():
-    return ResNet(BasicBlock, [7, 7, 7])
+def resnet44(num_classes=100):
+    return ResNet(BasicBlock, [7, 7, 7], num_classes=num_classes)
 
 
-def resnet56():
-    return ResNet(BasicBlock, [9, 9, 9])
+def resnet56(num_classes=100):
+    return ResNet(BasicBlock, [9, 9, 9], num_classes=num_classes)
 
 
-def resnet110():
-    return ResNet(BasicBlock, [18, 18, 18])
+def resnet110(num_classes=100):
+    return ResNet(BasicBlock, [18, 18, 18], num_classes=num_classes)
 
 
-def resnet1202():
-    return ResNet(BasicBlock, [200, 200, 200])
+def resnet1202(num_classes=100):
+    return ResNet(BasicBlock, [200, 200, 200], num_classes=num_classes)
