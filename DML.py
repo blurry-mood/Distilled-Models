3f25f28007cef144a976551c1ce9ea84f85cb70a
root <assis.ayoub@gmail.com>
2021-09-24 22:46:13 +0000

    todo: reproduce dml results

--- DML.py
+++ DML.py
@@ -1,3 +1,7 @@
+"""
+Example:
+    >>> python DML.py -d datasets/cifar-100/dataset -s models -g -1 -e 200 -b 64 -w 8 --nesterov
+"""
 from src.litmodels.dml import LitModel
 from src.datamodules.cifar100_datamodule import DataModule
 
@@ -12,50 +16,69 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--datapath", type=str, help='specify the path to cifar-100 dataset', required=True)
-parser.add_argument("-s", "--save", type=str, help='specify where to save trained models', required=True)
-parser.add_argument("-g", "--gpu", type=int, help='specify id of gpu to use; -1 denotes using all available gpus', default=-1, )
-parser.add_argument("-e", "--epochs", type=int, help='number of training epochs', default=200)
-parser.add_argument("-b", "--batch", type=int, help='batch size value', default=64)
-parser.add_argument("-w", "--workers", type=int, help='number of workers to use for loading data', default=4)
+
+parser.add_argument("--wandb", help='Whether or not to log metrics to WandB', action='store_true')
+parser.add_argument("--seed", type=int, help='Experiment seed, useful for results reproduction', default=1)
+
+parser.add_argument("-d", "--datapath", type=str, help='Specify the path to cifar-100 dataset', required=True)
+parser.add_argument("-s", "--save", type=str, help='Specify where to save trained models', required=True)
+parser.add_argument("-g", "--gpu", type=int, help='Specify id of gpu to use; -1 denotes using all available gpus', default=-1, )
+parser.add_argument("-e", "--epochs", type=int, help='Number of training epochs', default=200)
+parser.add_argument("-b", "--batch", type=int, help='Batch size value', default=64)
+parser.add_argument("-w", "--workers", type=int, help='Number of workers to use for loading data', default=4)
+parser.add_argument("-v", "--val_split", type=float, help='Portion of training set to use for validation', default=0.1)
+
+parser.add_argument("-l", "--lr", type=float, help='Initial learning rate for SGD', default=1e-1)
+parser.add_argument("-m", "--momentum", type=float, help='Momentum of SGD', default=.9)
+parser.add_argument("-W", "--weight_decay", type=float, help='Weight decay used for SGD', default=1e-3)
+parser.add_argument("-n", "--nesterov", help='Whether or not to use Nesterov in SGD', action='store_true')
+
+parser.add_argument("-S", "--step_size", type=int, help='Number of epochs to wait before reducing learning rate', default=60)
+parser.add_argument("-G", "--gamma", type=float, help='Learning rate decay coefficient. After every `step_size` epochs, the learning rate is mutliplied by gamma', default=0.1)
 
 args = parser.parse_args()
 
+pl.seed_everything(args.seed)
+
 train_transforms = T.Compose([
-                    T.RandomCrop(28, padding=4, padding_mode='reflect'),
-                    T.RandomHorizontalFlip(),  # randomly flip image horizontally
-                    T.ToTensor(),
+        T.RandomCrop(32, padding=4, padding_mode='reflect'),  
+        T.RandomHorizontalFlip(),
+        T.RandomRotation(degrees=15),  
+        T.ToTensor(),  
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  
                        ])
 
 test_transforms = T.Compose([
-                    T.ToTensor(),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  
                         ])
 
 
-def train(dataset_path:str, save_dir:str, gpu_id:int, max_epochs:int, batch_size:int, num_workers:int, ):
+def train(args):
     # datamodule & litmodel
-    dm = DataModule(dataset_path, train_transform=train_transforms, test_transform=test_transforms,
-                    batch_size=batch_size, num_workers=num_workers)
-    litmodel = LitModel()
+    dm = DataModule(args.datapath, train_transform=train_transforms, test_transform=test_transforms,
+                    batch_size=args.batch, num_workers=args.workers, val_split=args.val_split )
+    litmodel = LitModel(args.lr, args.momentum, args.weight_decay, args.step_size, args.gamma, args.nesterov)
 
     # logger & callback
     lr_monitor = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
-    logger = pl.loggers.wandb.WandbLogger(project='distilled models')
+    trainer_args = dict(callbacks=[lr_monitor], gpus=args.gpu, max_epochs=args.epochs, val_check_interval=1.,)
+    if args.wandb:
+        logger = pl.loggers.wandb.WandbLogger(project='distilled models')
+        trainer_args['logger'] = logger
 
     # trainer
-    trainer = pl.Trainer(callbacks=[lr_monitor], logger=logger, 
-                         gpus=gpu_id, max_epochs=max_epochs, 
-                         val_check_interval=1., )
+    trainer = pl.Trainer(**trainer_args)
 
     # train & test
     trainer.fit(litmodel, dm)
     trainer.test(litmodel)
     
     # save trained
-    torch.save(litmodel.resnet1.state_dict(), os.path.join(save_dir, 'dml_s1_resnet32.pth'))
-    torch.save(litmodel.resnet2.state_dict(), os.path.join(save_dir, 'dml_s2_resnet32.pth'))
+    torch.save(litmodel.resnet1.state_dict(), os.path.join(args.save, 'dml_s1_resnet32.pth'))
+    torch.save(litmodel.resnet2.state_dict(), os.path.join(args.save, 'dml_s2_resnet32.pth'))
 
     wandb.finish()
 
 if __name__=='__main__':
-    train(args.datapath, args.save, args.gpu, args.epochs, args.batch, args.workers)
+    train(args)
--- src/litmodels/dml.py
+++ src/litmodels/dml.py
@@ -19,60 +19,88 @@ def kl_div(x, y):
 
 class LitModel(pl.LightningModule):
     
-    def __init__(self, ):
+    def __init__(self, lr, momentum, weight_decay, step_size, gamma, nesterov):
         super().__init__()
         
         self.automatic_optimization = False
+        self.save_hyperparameters()
         
         self.resnet1 = resnet32()
         self.resnet2 = resnet32()
         
         self.celoss = nn.CrossEntropyLoss()
-        self.acc = Accuracy(num_classes=100, average='macro', compute_on_step=True)
+        self.top1 = Accuracy(num_classes=100, compute_on_step=True, top_k=1)
+        self.top5 = Accuracy(num_classes=100, compute_on_step=True, top_k=5)
         
     def configure_optimizers(self):
-        opt = torch.optim.SGD([*self.resnet1.parameters(), *self.resnet2.parameters()], lr=.1, momentum=.9, nesterov=True)
-        step = torch.optim.lr_scheduler.StepLR(opt, step_size=60, gamma=.1)
-        return [opt], [step]
+        hp = self.hparams
+        
+        opt1 = torch.optim.SGD(self.resnet1.parameters(), lr=hp.lr, momentum=hp.momentum, nesterov=hp.nesterov, weight_decay=hp.weight_decay)
+        opt2 = torch.optim.SGD(self.resnet2.parameters(), lr=hp.lr, momentum=hp.momentum, nesterov=hp.nesterov, weight_decay=hp.weight_decay)
+        step1 = torch.optim.lr_scheduler.StepLR(opt1, step_size=hp.step_size, gamma=hp.gamma, )
+        step2 = torch.optim.lr_scheduler.StepLR(opt2, step_size=hp.step_size, gamma=hp.gamma, )
+        return [opt1, opt2], [step1, step2]
     
-    def forward(self, x, optimize_first:bool=True):
+    def forward(self, x):
         x1 = self.resnet1(x)
         x2 = self.resnet2(x)
-        if not optimize_first:
-            x1 = x1.detach()
-        else:
-            x2 = x2.detach()
         return x1, x2
     
     def training_step(self, batch, batch_id):
         x, y = batch
 
-        opt = self.optimizers()
+        opt1, opt2 = self.optimizers()
         
         # Optimize student 1
         x1, x2 = self(x)
         loss = self.celoss(x1, y) + kl_div(x2, x1)
-        opt.zero_grad()
+        opt1.zero_grad()
         self.manual_backward(loss)
-        opt.step()
+        opt1.step()
         
         self.log('train_loss1', loss, prog_bar=True,)
         
         # Optimize student 2
-        x1, x2 = self(x, False)
+        x1, x2 = self(x)
         loss = self.celoss(x2, y) + kl_div(x1, x2)
-        opt.zero_grad()
+        opt2.zero_grad()
         self.manual_backward(loss)
-        opt.step()
+        opt2.step()
         
         self.log('train_loss2', loss, prog_bar=True,)
         
         if self.trainer.is_last_batch :
-            self.lr_schedulers().step()
-            
-    def test_step(self, batch, *a):
+            self.lr_schedulers()[0].step()
+            self.lr_schedulers()[1].step()
+    
+    def _step(self, batch, split):
         x, y = batch
-        x1, x2 = self(x, True)
+        x1, x2 = self(x)
+        
+        loss1 = self.celoss(x1, y)
+        loss2 = self.celoss(x2, y)
+
         x1, x2 = softmax(x1, dim=1), softmax(x2, dim=1)
-        self.log('test_acc1', self.acc(x1, y), prog_bar=True)
-        self.log('test_acc2', self.acc(x2, y), prog_bar=True)
+        
+        top11, top51 = self.top1(x1, y), self.top5(x1, y)
+        self.top1.reset()
+        self.top5.reset()
+        
+        top12, top52 = self.top1(x2, y), self.top5(x2, y)
+        self.top1.reset()
+        self.top5.reset()
+        
+        self.log(f'{split}_loss1', loss1, prog_bar=True)
+        self.log(f'{split}_loss2', loss2, prog_bar=True)
+        
+        self.log(f'{split}_top1_1', top11, prog_bar=True)
+        self.log(f'{split}_top1_2', top12, prog_bar=True)
+        self.log(f'{split}_top5_1', top51, prog_bar=True)
+        self.log(f'{split}_top5_2', top52, prog_bar=True)
+
+    def test_step(self, batch, *a):
+        self._step(batch, 'test')
+        
+
+    def validation_step(self, batch, *a):
+        self._step(batch, 'val')        
